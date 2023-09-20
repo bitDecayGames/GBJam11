7bfee91a8bb534bd6239f07428657d3089499ebe
Tanner <you@example.com>
2023-09-20 17:02:47 -0600

    ye

--- source/FmodConstants.hx
+++ source/FmodConstants.hx
@@ -12,9 +12,12 @@ class FmodSongs {
 class FmodSFX {
     public static inline var EnemyAlienDeath:String = "event:/SFX/Enemy/EnemyAlienDeath";
     public static inline var EnemyBossDamage:String = "event:/SFX/Enemy/EnemyBossDamage";
+    public static inline var EnemyBossDamage2:String = "event:/SFX/Enemy/EnemyBossDamage2";
     public static inline var MenuHover:String = "event:/SFX/Menu/MenuHover";
     public static inline var MenuSelect:String = "event:/SFX/Menu/MenuSelect";
     public static inline var PlayerDamage:String = "event:/SFX/Player/Damage/PlayerDamage";
+    public static inline var PlayerDuck:String = "event:/SFX/Player/DuckUnduck/PlayerDuck";
+    public static inline var PlayerUnduck:String = "event:/SFX/Player/DuckUnduck/PlayerUnduck";
     public static inline var PlayerJump:String = "event:/SFX/Player/Jump/PlayerJump";
     public static inline var PlayerLand:String = "event:/SFX/Player/Land/PlayerLand";
     public static inline var PlayerStep:String = "event:/SFX/Player/Step/PlayerStep";
